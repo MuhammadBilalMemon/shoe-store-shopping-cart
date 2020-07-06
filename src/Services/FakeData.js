f1e29d9904b9afa2ca7687dfe212bf0d2bfbbf3e
Muhammad Bilal <billu97.memon@gmail.com>
2020-07-06 06:07:57 -0700

    Created FakeData.js file

---
+++ src/Services/FakeData.js
@@ -0,0 +1,75 @@
+export const SampleData = [
+    {
+        productId: 1,
+        productName: "Nike Blazer",
+        productPrice: 100,
+        img: "",
+        slug: "nike-blazer",
+    },
+    {
+        productId: 2,
+        productName: "Nike Air Zoom Pegasus",
+        productPrice: 120,
+        img: "",
+        slug: "nike-air-zoom-pegasus",
+    },
+    {
+        productId: 3,
+        productName: "LeBron 17 Graffiti",
+        productPrice: 255,
+        img: "",
+        slug: "lebron-17-graffiti",
+    }, {
+        productId: 4,
+        productName: "Air Jordan 3 Retro SE",
+        productPrice: 250,
+        img: "",
+        slug: "air-jordan-3-retro",
+    },
+    {
+        productId: 5,
+        productName: "Nike React Phantom",
+        productPrice: 140,
+        img: "",
+        slug: "nike-react-phantom",
+    },
+    {
+        productId: 6,
+        productName: "Nike Free Metcon",
+        productPrice: 130,
+        img: "",
+        slug: "nike-free-metcon",
+    }
+    ,
+    {
+        productId: 7,
+        productName: "Nike Air Max Alpha",
+        productPrice: 110,
+        img: "",
+        slug: "nike-air-max-alpha",
+    }
+    ,
+    {
+        productId: 8,
+        productName: "Nike Metcon 5",
+        productPrice: 97,
+        img: "",
+        slug: "Nike-Metcon-5",
+    }
+    ,
+    {
+        productId: 9,
+        productName: "Nike Air Max Alpha TR 3",
+        productPrice: 85,
+        img: "",
+        slug: "nike-air-max-alpha",
+    }
+    ,
+    {
+        productId: 10,
+        productName: "Nike Reax 8 TR",
+        productPrice: 67,
+        img: "",
+        slug: "nike-reax",
+    }
+]
